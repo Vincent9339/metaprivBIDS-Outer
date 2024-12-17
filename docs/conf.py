bd12cd0723a85d2e9b5ed106234aa498b5b22812
Emilie Kibsgaard <em_kibsgaard@outlook.dk>
2024-12-17 10:26:16 +0100

    read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,16 +3,29 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
+
+# Insert sys.path modification and mock code here
 sys.path.insert(0, os.path.abspath('metaprivBIDS/metaprivBIDS/metaprivBIDS.py'))
 
+# Mocking rpy2 to prevent build failures on Read the Docs
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+# List of modules to mock
+MOCK_MODULES = ['rpy2', 'rpy2.robjects', 'rpy2.rinterface']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = 'metaprivBIDS'
-copyright = '2024, Author'
-author = 'Author'
+copyright = '2024, Emilie B. Kibsgaard'
+author = 'Emilie B. Kibsgaard'
 release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
@@ -22,8 +35,6 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
